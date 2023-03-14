f0a3956521934e4a8f4d217404e1f9133f5244e4
viktor111 <swifrorlilko@gmail.com>
2023-03-14 14:20:21 +0200

    ADD: ImageDataValidator

---
+++ CoffeePalace.Services/Validators/ImageDataValidator.cs
@@ -0,0 +1,33 @@
+using CoffeePalace.Models.Entities;
+using CoffeePalace.Services.Common;
+
+using static CoffeePalace.Models.Constants.ImageDataConstants;
+
+namespace CoffeePalace.Services.Validators;
+
+public class ImageDataValidator
+{
+    public Result Validate()
+    {
+        try
+        {
+            return Result.Success;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+
+    private void ValidateName(string name)
+    {
+        var nameProp = nameof(ImageData.Name);
+        
+        if (string.IsNullOrWhiteSpace(name)) 
+            throw new Exception(ErrorMessageBuilder.NullWhiteSpaceOrEmpty(nameof(nameProp)));
+        if (name.Length < MinNameLen) 
+            throw new Exception(ErrorMessageBuilder.MinLen(nameProp, MinNameLen));
+        if (name.Length > MaxNameLen) 
+            throw new Exception(ErrorMessageBuilder.MaxLen(nameProp, MaxNameLen));
+    }
+}
